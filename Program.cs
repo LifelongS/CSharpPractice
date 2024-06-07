5ccffaa8811803740dd43ae472b224ba204d1811
LifelongS <i.l.kuchina@yandex.ru>
2024-06-07 16:28:10 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿public partial class MainClass
+{
+    public double Add(double a, double b)
+    { 
+        return a + b; 
+    }
+    static void Main(string[] args)
+    {     
+        double a = 1;    
+        double b = 2;    
+        MainClass mainClass = new MainClass();    
+        double result = mainClass.Add(a, b);    
+        Console.WriteLine(result);
+    }
+ }
